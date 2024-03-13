e9765c31ee9f83b2903a6fbb6e95f205665c1fc9
Filip <filfer05@gmail.com>
2024-03-13 15:12:41 +0100

    contact layout fix

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,5 +1,5 @@
 import Contact from "@/components/home/contact";
 
 export default function Page() {
-  return <Contact className="min-h-[calc(100vh-207px)]" />;
+  return <Contact className="min-h-[calc(100vh-191px)]" />;
 }
