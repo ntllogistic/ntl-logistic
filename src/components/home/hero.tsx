df1ace4b765d6281f003a408bc61afca97d95641
Filip <filfer05@gmail.com>
2024-03-13 15:54:21 +0100

    hero gradient

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -4,16 +4,17 @@ import Button from "../ui/button";
 import { inter } from "@/assets/font/monument-extended";
 import Gradient from "../ui/gradient";
 import Link from "next/link";
+import Title from "./title";
 
 export default function Hero() {
   return (
     <section className="px-4 flex flex-col gap-4 md:grid grid-cols-3 bg-background">
-      <div className="col-span-3 relative overflow-hidden rounded-lg">
-        <div className="bg-gradient-to-r from-[rgba(25,25,25,.9)] to-[rgba(25,25,25,.5)] sm:from-[rgba(25,25,25,.8)] sm:to-[rgba(25,25,25,.2)] w-full h-full px-8 md:px-[4vw] 2xl:px-[12vw] py-32 lg:py-64 relative z-10">
+      <div className="col-span-3 relative overflow-hidden rounded-lg sm:rounded-l-none">
+        <div className="bg-gradient-to-r from-[rgba(25,25,25,.97)] to-[rgba(25,25,25,.6)] sm:from-[rgba(25,25,25,1)] sm:to-[rgba(25,25,25,.4)] w-full h-full px-8 md:px-[4vw] 2xl:px-[12vw] py-32 lg:py-64 relative z-10">
           <div className="flex flex-col gap-6 relative z-10">
             <h1 className="text-3xl lg:text-4xl text-white font-semibold leading-snug lg:leading-snug">
               Innowacyjny Transport
-              <br /> Nowa Jakość w Logistyce
+              <br /> Nowa jakość w Logistyce
             </h1>
             <p
               className={`text-white/80 leading-relaxed lg:leading-relaxed text-sm lg:text-base max-w-xl ${inter.className}`}
@@ -35,14 +36,12 @@ export default function Hero() {
           alt="Ciężarówka jadące po autostradzie"
         />
       </div>
-      <div className="py-24 md:py-36 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
-        <div className="space-y-4 text-center relative z-10">
+      <div className="py-24 md:py-32 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
+        <div className="space-y-3 text-center relative z-10">
           <h2 className="text-xl lg:text-2xl text-white font-medium">
             Transport
           </h2>
-          <p
-            className={`text-white/90 text-sm lg:text-base  ${inter.className}`}
-          >
+          <p className={`text-white/90 text-sm  ${inter.className}`}>
             Pewność i bezpieczeństwo
           </p>
         </div>
@@ -53,16 +52,14 @@ export default function Hero() {
           src={transport.src}
           alt="Transport"
         />
-        <Gradient className="w-full absolute bottom-0" />
+        <Gradient className="w-full absolute -bottom-16" />
       </div>
-      <div className="py-24 md:py-36 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
-        <div className="space-y-4 text-center relative z-10">
+      <div className="py-24 md:py-32 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
+        <div className="space-y-3 text-center relative z-10">
           <h2 className="text-xl lg:text-2xl text-white font-medium">
             Spedycja
           </h2>
-          <p
-            className={`text-white/90 text-sm lg:text-base  ${inter.className}`}
-          >
+          <p className={`text-white/90 text-sm  ${inter.className}`}>
             Profesjonalna organizacja
           </p>
         </div>
@@ -73,16 +70,14 @@ export default function Hero() {
           src={shipping.src}
           alt="Spedycja"
         />
-        <Gradient className="w-full absolute bottom-0" />
+        <Gradient className="w-full absolute -bottom-16" />
       </div>
-      <div className="py-24 md:py-36 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
-        <div className="space-y-4 text-center relative z-10">
+      <div className="py-24 md:py-32 px-6 flex items-center justify-center relative rounded-lg overflow-hidden">
+        <div className="space-y-3 text-center relative z-10">
           <h2 className="text-xl lg:text-2xl text-white font-medium">
             Obsługa celna
           </h2>
-          <p
-            className={`text-white/90 text-sm lg:text-base  ${inter.className}`}
-          >
+          <p className={`text-white/90 text-sm  ${inter.className}`}>
             Pomoc w czynnościach celnych i promowych
           </p>
         </div>
@@ -93,7 +88,7 @@ export default function Hero() {
           src={service.src}
           alt="Obsługa celna"
         />
-        <Gradient className="w-full absolute bottom-0" />
+        <Gradient className="w-full absolute -bottom-16" />
       </div>
     </section>
   );
